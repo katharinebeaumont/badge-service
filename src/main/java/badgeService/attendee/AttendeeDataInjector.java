f8924d96462e4ada8be1ea84fde55303a068b240
Sylvain Jermini <sylvain.jermini@syjer.com>
2017-10-31 11:48:23 +0100

    save remote data (incomplete)

--- src/main/java/badgeService/attendee/AttendeeDataInjector.java
+++ src/main/java/badgeService/attendee/AttendeeDataInjector.java
@@ -4,9 +4,14 @@ import badgeService.util.CSVParser;
 import badgeService.util.EventPathProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.env.Environment;
+import org.springframework.http.*;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -17,16 +22,38 @@ public class AttendeeDataInjector implements CommandLineRunner {
 
     private final AttendeeRepository attendeeRepository;
     private final EventPathProperties properties;
+    private final Environment environment;
+    private final RestTemplate restTemplate;
 
     @Autowired
-    public AttendeeDataInjector(AttendeeRepository attendeeRepository, EventPathProperties properties) {
+    public AttendeeDataInjector(AttendeeRepository attendeeRepository, EventPathProperties properties, Environment environment) {
         this.attendeeRepository = attendeeRepository;
         this.properties = properties;
+        this.environment = environment;
+        this.restTemplate = new RestTemplate();
     }
 
     @Override
     public void run(String... strings) throws Exception {
-        generateAndSaveAttendees();
+        String source = environment.getProperty("attendee.datasource");
+        if("csv".equals(source)) {
+            generateAndSaveAttendees();
+        } else if ("remote".equals(source)){
+            loadAttendeeFromRemote(environment.getProperty("attendee.datasource.url"),
+                    environment.getProperty("attendee.datasource.header.name"),
+                    environment.getProperty("attendee.datasource.header.value"));
+        }
+    }
+
+    //TODO add job and save timestamp, so we can discard the older one
+    private void loadAttendeeFromRemote(String url, String headerName, String headerValue) {
+        HttpHeaders h = new HttpHeaders();
+        h.set(headerName, headerValue);
+
+        ResponseEntity<List<RemoteAttendee>> res = restTemplate.exchange(url, HttpMethod.GET, new HttpEntity(h), new ParameterizedTypeReference<List<RemoteAttendee>>() {});
+        List<Attendee> attendees = res.getBody().stream().map(remoteAttendee -> new Attendee(remoteAttendee.getRegistrationCode(), remoteAttendee.getFirstName(), remoteAttendee.getLastName(), remoteAttendee.getEmail())).collect(Collectors.toList());
+        attendeeRepository.save(attendees);
+        Stream.of((attendeeRepository.findAll())).forEach(attendee -> System.out.print(attendee.toString()));
     }
 
     public void generateAndSaveAttendees() {
